0948bb4c72cc1c50276837a97e6501afabd8fe9c
Yahya <92686055+ya7yaq@users.noreply.github.com>
2021-12-19 15:33:30 +0300

    cnt

---
+++ BackEnd/server.js
@@ -0,0 +1,19 @@
+// setup server exporess and import db
+const express = require("express");
+const app = express();
+const db = require("./db");
+
+// port var
+const port = 4000;
+
+// get post ... methods
+
+app.get("/", (req, res) => {
+  res.json("GET Working on /");
+});
+
+// app listen
+
+app.listen(PORT, () => {
+  console.log(`${PORT} is working ..`);
+});

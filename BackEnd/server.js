27f398012c6c4707b2262d2caa4c25928282ef0e
Yahya <92686055+ya7yaq@users.noreply.github.com>
2021-12-20 11:19:43 +0300

    finish put and delete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -40,6 +40,42 @@ app.post("/tasks", (req, res) => {
   });
 });
 
+// delete
+app.delete("/tasks/:id", (req, res) => {
+  // console.log(req.params);
+  Todo.deleteOne({ _id: req.params.id }, (err, deleteObj) => {
+    if (err) {
+      console.log("ERROR", err);
+      res.status(500).json("there is a problem in DB");
+    } else {
+      // console.log(deleteObj)
+      if (deleteObj.deletedCount === 0) {
+        res.status(404).json("User Not Found");
+      } else {
+        res.status(200).json("Success Delete " + req.params.id);
+      }
+    }
+  });
+});
+
+// put
+app.put("/tasks/:id", (req, res) => {
+  // console.log(req.params);
+  Todo.updateOne({ _id: req.params.id }, { title: req.body.newTitle }, (err, updateObj) => {
+    if (err) {
+      console.log("ERROR", err);
+      res.status(500).json("there is a problem in DB");
+    } else {
+      // console.log(updateObj)
+      if (updateObj.matchedCount === 0) {
+        res.status(404).json("User Not Found");
+      } else {
+        res.status(200).json("Success Update one user");
+      }
+    }
+  });
+});
+
 // app listen
 
 app.listen(PORT, () => {
